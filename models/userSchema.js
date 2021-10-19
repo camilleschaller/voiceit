dd86c79bd7f7a69ce389ba9443cdc389b1184eb8
camilleschaller <camille.schaller@heig-vd.ch>
2021-10-19 15:35:28 +0200

    Update userschema

--- models/userSchema.js
+++ models/userSchema.js
@@ -6,6 +6,16 @@ const userSchema = new Schema({
   pseudo: {
     type: String,
     required: true,
+    minlength: 3,
+    maxlength: 30,
+    unique: true,
+    validate:
+      // Manually validate uniqueness to send a "pretty" validation error
+      // rather than a MongoDB duplicate key error
+      [{
+        validator: validateUserPseudoUniqueness,
+        message:'User {VALUE} already exists'
+      }],
   },
   password: {
     type: String,
@@ -13,14 +23,24 @@ const userSchema = new Schema({
   },
   mail: {
     type: String,
-    required: true
+    required: true,
+    format: email
   }
-
 });
 
 userSchema.set('toJSON', {
   transform: transformJsonUser
 });
+
+/**
+ * Given a pseudo, calls the callback function with true if no user exists with that name
+ * (or the only user that exists is the same as the user being validated).
+ */
+ function validateUserPseudoUniqueness(value) {
+  return this.constructor.findOne().where('pseudo').equals(value).exec().then((existingUser) => {
+    return !existingUser || existingUser._id.equals(this._id);
+  });
+}
 
 function transformJsonUser(doc, json, options) {
  //Remove the hashed password from the generated JSON.
